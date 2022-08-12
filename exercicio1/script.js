d375a08e7b8987276411e0993d339d9c340625d9
JenniferCastro <jennifer.erc@gmail.com>
2022-08-12 12:44:27 -0400

    exercícios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,11 @@
-const num = //seu numero aqui
+/*# Exercício 1
 
-if(){
-    console.log("sua mensagem aqui");
+Crie um código que receba uma const numérica qualquer. Crie um `if` para verificar se o número guardado na const é **par**. Caso seja, imprima no console a mensagem: "o número é par".*/
+
+const num = Number(prompt('Digite um número'))
+const total = num % 2
+if(total === 0) {
+    console.log("Seu número é par");
+}else if(total !== 0){
+    console.log("Seu número é ímpar");
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,46 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+/*# Exercício 2
 
-if(){
+Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio, e um booleano que responda se a pessoa está cursando alguma faculdade.
 
+Crie um `if` para cada variável, checando as seguintes afirmacões:
+- Se a pessoa tem 18 anos ou mais;
+- Se a pessoa terminou o ensino médio;
+- Se a pessoa NÃO está cursando alguma faculdade;
+
+Crie um `if` que imprima que a afirmacão é verdadeira, e um `else` para imprimir que a afirmacão não é verdadeira.
+
+<details>
+
+<summary>Exemplo</summary>
+
+Caso a primeira pessoa tenha idade >=18:
+
+<b>"A pessoa é maior de idade"</b>
+
+e caso não seja:
+
+<b>"A pessoa é menor de idade"</b>
+
+</details>*/
+
+
+
+let idade = 18
+
+if(idade >= 18){
+  console.log('Você é maior de idade')
 }else{
+    console.log('Você ainda não é maior de idade, aproveite rs')
 	
 }
+
+let ensinoMedio = prompt('Você terminou o ensino médio?')
+
+if(ensinoMedio === 1){
+    console.log('')
+
+}
+
+
+
+
